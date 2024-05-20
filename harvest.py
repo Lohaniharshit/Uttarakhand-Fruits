21738afa566c082dbeda87a98cde4fc17ebb2155
nivesh <nivesh.nk1000@gmail.com>
2024-05-20 14:56:01 +0530

    harvest.py added

--- harvest.py
+++ harvest.py
@@ -1,2 +1,22 @@
 import csv
+from SoiltypeRatio import Soiltype
+from givefruitforregion import get_fruits_by_region
+from Monthly_Fruit_Growth import each_fruit
 
+
+def main():
+    file_path = 'Dataset/Fruits(final).csv'
+    data=[]
+    with open(file_path, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+
+    query = '  RISHIKESH'
+
+
+    each_fruit(data, query)
+
+
+if __name__ == "__main__":
+    main()
