149b1a744c27497056dd61039a950add96b504c1
nivesh <nivesh.nk1000@gmail.com>
2024-05-20 14:35:54 +0530

    fixed some errors

--- SoiltypeRatio.py
+++ SoiltypeRatio.py
@@ -1,22 +1,35 @@
-import csv
 import matplotlib.pyplot as plt
 
-# Open the CSV file
-with open('Dataset\Fruits(final).csv','r') as csvfile:
-    # Create a CSV reader object
-    reader = csv.DictReader(csvfile)
-    loamy=0
-    sandy=0
-    clayey=0
-    for i in reader:
-        if(i['Soil Type']=='Loamy'):
-            loamy+=1
-        elif(i['Soil Type']=='Sandy'):
-            sandy+=1
-        else:
-            clayey+=1
-    labels=['Loamy','Sandy','Clayey']
-    ratios=[loamy,sandy,clayey]
-    plt.pie(ratios,labels=labels,autopct='%1.1f%%')
-    plt.title('Soil wise Distribution of Fruits')
+
+def Soiltype(data,):
+    """
+    Analyzes the distribution of different soil types from the given data and
+    visualizes the distribution as a pie chart.
+
+    Parameters:
+    data (list of dict): A list of dictionaries where each dictionary
+    represents a data entry with a 'Soil Type' key.
+    query (str, optional): An optional parameter for future extensibility.
+    Currently not used in this function.
+
+    Returns:
+    None
+    """
+    loamy = 0
+    sandy = 0
+    clayey = 0
+
+    for entry in data:
+        if entry['Soil Type'] == 'Loamy':
+            loamy += 1
+        elif entry['Soil Type'] == 'Sandy':
+            sandy += 1
+        elif entry['Soil Type'] == 'Clayey':
+            clayey += 1
+
+    labels = ['Loamy', 'Sandy', 'Clayey']
+    ratios = [loamy, sandy, clayey]
+
+    plt.pie(ratios, labels=labels, autopct='%1.1f%%')
+    plt.title('Soil-wise Distribution of Fruits')
     plt.show()
